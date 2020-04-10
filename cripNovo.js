538435b96e977229439e7015ddaf094aad89613d
Carol Quiterio <carolquiterio@yahoo.com.br>
2020-04-09 23:49:48 -0300

    Second commit

--- cripNovo.js
+++ cripNovo.js
@@ -3,130 +3,72 @@ criptografia.frase = '';
 criptografia.fraseCriptografada = '';
 
 criptografia.crip = function() { };
-criptografia.crip2 = function() { };
 
-function codificar(letra)
+var simbolos = "abcdefghijklmnoprstuvwxyz";
+var tamanho = simbolos.length;
+
+function codificar(letra) {
+    for (i = 0; i < tamanho; i++) {
+
+        if (letra === "z") {
+            return "a";
+        }
+
+        if (letra === " ") {
+            return " ";
+        }
+
+        if(letra === this.simbolos.substring(i, i + 1)) { 
+            return simbolos.substring(i + 1, i + 2);
+        }
+    }   
+}
+
+function descodificar(letra)
 {
-    switch(letra)
+    if (letra === "a") {
+        return "z";
+    }
+
+    if (letra === " ") {
+        return " ";
+    }
+    for(i = 0; i < tamanho; i++)
     {
-        case 'A': return 'Q';break;
-        case 'S': return 'W';break;
-        case 'D': return 'E';break;
-        case 'F': return 'R';break;
-        case 'G': return 'T';break;
-        case 'H': return 'Y';break;
-        case 'J': return 'U';break;
-        case 'K': return 'I';break;
-        case 'L': return 'O';break;
-        case 'Ç': return 'P';break;
-        case 'Q': return '1';break;
-        case 'W': return '2';break;
-        case 'E': return '3';break;
-        case 'R': return '4';break;
-        case 'T': return '5';break;
-        case 'Y': return '6';break;
-        case 'U': return '7';break;
-        case 'I': return '8';break;
-        case 'O': return '9';break;
-        case 'P': return '0';break;
-        case '1': return 'Z';break;
-        case '2': return 'X';break;
-        case '3': return 'C';break;
-        case '4': return 'V';break;
-        case '5': return 'B';break;
-        case '6': return 'N';break;
-        case '7': return 'M';break;
-        case '8': return 'A';break;
-        case '9': return 'S';break;
-        case '0': return 'D';break;
-        case 'Z': return 'F';break;
-        case 'X': return 'G';break;
-        case 'C': return 'H';break;
-        case 'V': return 'J';break;
-        case 'B': return 'K';break;
-        case 'N': return 'L';break;
-        case 'M': return 'Ç';break;
-        case ' ': return ' ';break;
+        if (letra === this.simbolos.substring(i, i - 1))
+        {
+            return simbolos.substring(i - 1, i - 2);
+        }
     }
 }
 
-function codificar2(letra)
-{
-    switch(letra)
-    {
-        case 'A': return 'Z';break;
-        case 'S': return 'X';break;
-        case 'D': return 'C';break;
-        case 'F': return 'V';break;
-        case 'G': return 'B';break;
-        case 'H': return 'N';break;
-        case 'J': return 'M';break;
-        case 'K': return 'A';break;
-        case 'L': return 'S';break;
-        case 'Ç': return 'D';break;
-        case 'Q': return 'F';break;
-        case 'W': return 'G';break;
-        case 'E': return 'H';break;
-        case 'R': return 'J';break;
-        case 'T': return 'K';break;
-        case 'Y': return 'L';break;
-        case 'U': return 'Ç';break;
-        case 'I': return 'Q';break;
-        case 'O': return 'W';break;
-        case 'P': return 'E';break;
-        case '1': return 'R';break;
-        case '2': return 'T';break;
-        case '3': return 'Y';break;
-        case '4': return 'U';break;
-        case '5': return 'I';break;
-        case '6': return 'O';break;
-        case '7': return 'P';break;
-        case '8': return '1';break;
-        case '9': return '2';break;
-        case '0': return '3';break;
-        case 'Z': return '4';break;
-        case 'X': return '5';break;
-        case 'C': return '6';break;
-        case 'V': return '7';break;
-        case 'B': return '8';break;
-        case 'N': return '9';break;
-        case 'M': return '0';break;
-        case ' ': return ' ';break;
-    }
-}
-
-
-function Criptografia(f1) 
-{
+function Criptografia(f1) {
     this.frase = f1
 
-    this.crip = function()  
-    {   
+    this.crip = function() {   
         var codigo = "";
-        for(indice in this.frase)
-        {   
+        for(indice in this.frase) {   
             var letra = this.frase[indice];
-            var letraCodificada  = codificar(letra.toUpperCase());
+            var letraCodificada  = codificar(letra.toLowerCase());
             codigo += letraCodificada;
         }
 
         return codigo;
     }
-    this.crip2 = function()  
-    {   
+    
+    this.descrip = function() {   
         var codigo = "";
-        for(indice in this.frase)                                             
-        {                                                                                            
-           var letra = this.frase[indice];
-           var letraCodificada  = codificar2(letra.toUpperCase());
-           codigo += letraCodificada; 
-        }                                                       
-                                                                                
+        for(indice in this.frase) {   
+            var letra = this.frase[indice];
+            var letraCodificada  = descodificar(letra.toLowerCase());
+            codigo += letraCodificada;
+        }
+
         return codigo;
     }
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
     $("input[name=botao]").click(function()  {  
         
         var conteudoBotao = $(this).val();
@@ -144,7 +86,7 @@ $(document).ready(function(){
 
      $("input[name=botaocr2]").click(function(){
          var obj_frase = new Criptografia($("#frase").val());
-         $("#frase").val(obj_frase.crip2()); 
+         $("#frase").val(obj_frase.descrip()); 
      }); 
 
 });
